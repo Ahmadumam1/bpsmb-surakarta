2e2016ddc0480dc23edea7c49fa5d0d0452314cc
Ahmadumam1 <ahmadumam246@gmail.com>
2026-07-13 21:42:12 +0700

    Configure dynamic public path for cPanel deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,3 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// Mengarahkan folder public ke public_html secara dinamis untuk cPanel
+if (file_exists(dirname(dirname(__DIR__)) . '/public_html')) {
+    $app->usePublicPath(dirname(dirname(__DIR__)) . '/public_html');
+}
+
+return $app;
